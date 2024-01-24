b6067ac151f3bd575e9d672eb8e60b4c730e1e75
dlss84 <danielsebastianis84@gmail.com>
2024-01-23 20:44:16 -0500

    estructura OK

--- src/components/PasswordGenerator/PasswordGenerator.jsx
+++ src/components/PasswordGenerator/PasswordGenerator.jsx
@@ -22,6 +22,46 @@ function PasswordGenerator() {
           </div>
         </div>
       </div>
+      <div className="setting">
+        <h3>Personaliza tu contraseña</h3>
+        <div className="customize">
+          <div className="checkboxes">
+            <div className="left">
+              <div className="checkbox-field">
+                <input type="checkbox" name="lower" id="lower" />
+                <label htmlFor="lower">Incluir Letras Minúsculas (a-z)</label>
+              </div>
+              <div className="checkbox-field">
+                <input type="checkbox" name="upper" id="upper" />
+                <label htmlFor="upper">Incluir Letras Mayúsculas (A-Z)</label>
+              </div>
+            </div>
+            <div className="right">
+              <div className="checkbox-field">
+                <input type="checkbox" name="numbers" id="numbers" />
+                <label htmlFor="numbers">Incluir Números (0-9)</label>
+              </div>
+              <div className="checkbox-field">
+                <input type="checkbox" name="symbols" id="symbols" />
+                <label htmlFor="symbols">Incluir Simbolos (&-#)</label>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="password-length">
+        <h3>Longitud de Contraseña</h3>
+        <div className="slider">
+          <p className="rangeValue">10</p>
+          <div className="range">
+            <input type="range" min={10} max={40} />
+          </div>
+        </div>
+      </div>
+      <div className="buttons">
+        <button type="button">Copiar Contraseña</button>
+        <button type="button" >Generar Contraseña</button>
+      </div>
     </div>
   );
 }
